1a1beb400e212c78cbcaeee31038ddac2ddb5552
Rasmus Hellström <hellstromrasmus@gmail.com>
2024-04-08 18:41:12 +0300

    As StringCalculators responsibility grew, I made add() a free standing function and modified unit tests

--- string-calculator/src/main.rs
+++ string-calculator/src/main.rs
@@ -1,27 +1,25 @@
+use std::io;
+
 mod string_calculator;
 
-use std::io;
-use std::io::Write;
-use crate::string_calculator::StringCalculator;
-
 fn main() -> io::Result<()>{
-    let mut buffer = String::new();
-    let calculator = StringCalculator;
-
-    //Loop until the user inputs "exit"
-    loop {
-        print!("? ");
-        io::stdout().flush()?;
-
-        buffer.clear();
-        io::stdin().read_line(&mut buffer)?;
-        if buffer.trim().eq_ignore_ascii_case("exit"){
-            break;
-        }
-
-        let result = calculator.add(&buffer);
-        println!("{:?}", result);
-    }
-    println!("Exiting");
+    // let mut buffer = String::new();
+    // let calculator = StringCalculator;
+    // 
+    // //Loop until the user inputs "exit"
+    // loop {
+    //     print!("? ");
+    //     io::stdout().flush()?;
+    // 
+    //     buffer.clear();
+    //     io::stdin().read_line(&mut buffer)?;
+    //     if buffer.trim().eq_ignore_ascii_case("exit"){
+    //         break;
+    //     }
+    // 
+    //     let result = calculator.add(&buffer);
+    //     println!("{:?}", result);
+    // }
+    // println!("Exiting");
     Ok(())
 }
--- string-calculator/src/string_calculator.rs
+++ string-calculator/src/string_calculator.rs
@@ -1,72 +1,80 @@
-pub struct StringCalculator;
-
-
+#[allow(dead_code)]
 #[derive(Debug, PartialEq)]
 pub enum CalculatorError {
     NegativeNumber(i32),
 }
-impl StringCalculator {
-    pub fn add(&self, input: &str) -> Result<i32, CalculatorError> {
-        let mut sum = 0;
-        let mut delim = ',';
-        
-        if input.starts_with("//"){
-            if let Some(new_delim) = input.chars().nth(2) {
-                delim = new_delim;
-            }
+
+pub trait Logger {
+    fn log(&self, number: i32);
+}
+
+#[allow(dead_code)]
+pub struct StringCalculator<'a> {
+    logger: &'a dyn Logger,
+}
+
+#[allow(dead_code)]
+impl<'a> StringCalculator<'a> {
+    pub fn new(logger: &'a (dyn Logger + 'a)) -> StringCalculator {
+        Self{
+            logger
         }
-            
-        for item in input.split(|c: char| c == delim || c == '\n') {
-            if let Ok(number) = item.parse::<i32>(){
-                if number < 0 {
-                    return Err(CalculatorError::NegativeNumber(number));
-                }
-                sum += number;
-            }
-        }
-        Ok(sum)
     }
+}
+
+#[allow(dead_code)]
+fn add(input: &str) -> Result<i32, CalculatorError> {
+    let mut sum = 0;
+    let mut delim = ',';
+
+    if input.starts_with("//"){
+        if let Some(new_delim) = input.chars().nth(2) {
+            delim = new_delim;
+        }
+    }
+
+    for item in input.split(|c: char| c == delim || c == '\n') {
+        if let Ok(number) = item.parse::<i32>(){
+            if number < 0 {
+                return Err(CalculatorError::NegativeNumber(number));
+            }
+            sum += number;
+        }
+    }
+    Ok(sum)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::string_calculator::StringCalculator;
+    use crate::string_calculator::{add};
     use crate::string_calculator::CalculatorError;
     #[test]
     fn test_empty_string_returns_zero() {
-        let calculator = StringCalculator;
-        assert_eq!(0, calculator.add("").unwrap());
+        assert_eq!(0, add("").unwrap());
     }
     #[test]
     fn test_one_number_input() {
-        let calculator = StringCalculator;
-        assert_eq!(44, calculator.add("44").unwrap());
+        assert_eq!(44, add("44").unwrap());
     }
     #[test]
     fn test_two_number_input() {
-        let calculator = StringCalculator;
-        assert_eq!(5, calculator.add("2,3").unwrap());
+        assert_eq!(5, add("2,3").unwrap());
     }
     #[test]
     fn test_unknown_number_input() {
-        let calculator = StringCalculator;
-        assert_eq!(55, calculator.add("1,2,3,4,5,6,7,8,9,10").unwrap());
+        assert_eq!(55, add("1,2,3,4,5,6,7,8,9,10").unwrap());
     }
     #[test]
     fn test_handle_newlines_input() {
-        let calculator = StringCalculator;
-        assert_eq!(6, calculator.add("1\n2,3").unwrap());
+        assert_eq!(6, add("1\n2,3").unwrap());
     }
     #[test]
     fn test_various_delimiters_input() {
-        let calculator = StringCalculator;
-        assert_eq!(3, calculator.add("//;\n1;2").unwrap());
+        assert_eq!(3, add("//;\n1;2").unwrap());
     }
     #[test]
     fn test_negative_number_input() {
-        let calculator = StringCalculator;
-        let result = calculator.add("1,-2,3");
-
+        let result = add("1,-2,3");
         assert_eq!(result, Err(CalculatorError::NegativeNumber(-2)),
         "Negative number should result in error");
     }
