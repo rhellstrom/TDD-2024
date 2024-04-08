0208f0b81005d5e1ec2568c26679b78cf56e8e5e
Rasmus Hellström <hellstromrasmus@gmail.com>
2024-04-08 19:12:28 +0300

    Stringcalculator Impl add() again as it made more sense for the rest of the assignment. Added struct creation and MockLogger to unit tests instead

--- string-calculator/src/string_calculator.rs
+++ string-calculator/src/string_calculator.rs
@@ -1,80 +1,96 @@
+use mockall::automock;
+
 #[allow(dead_code)]
 #[derive(Debug, PartialEq)]
 pub enum CalculatorError {
     NegativeNumber(i32),
 }
 
+#[automock]
 pub trait Logger {
     fn log(&self, number: i32);
 }
 
-#[allow(dead_code)]
-pub struct StringCalculator<'a> {
-    logger: &'a dyn Logger,
+pub struct StringCalculator<L: Logger> {
+    logger: L,
 }
 
 #[allow(dead_code)]
-impl<'a> StringCalculator<'a> {
-    pub fn new(logger: &'a (dyn Logger + 'a)) -> StringCalculator {
-        Self{
+impl<L: Logger> StringCalculator<L> {
+    pub fn new(logger: L) -> Self {
+        Self {
             logger
         }
     }
-}
 
-#[allow(dead_code)]
-fn add(input: &str) -> Result<i32, CalculatorError> {
-    let mut sum = 0;
-    let mut delim = ',';
+    fn add(&self, input: &str) -> Result<i32, CalculatorError> {
+        let mut sum = 0;
+        let mut delim = ',';
 
-    if input.starts_with("//"){
-        if let Some(new_delim) = input.chars().nth(2) {
-            delim = new_delim;
-        }
-    }
-
-    for item in input.split(|c: char| c == delim || c == '\n') {
-        if let Ok(number) = item.parse::<i32>(){
-            if number < 0 {
-                return Err(CalculatorError::NegativeNumber(number));
+        if input.starts_with("//") {
+            if let Some(new_delim) = input.chars().nth(2) {
+                delim = new_delim;
             }
-            sum += number;
         }
+
+        for item in input.split(|c: char| c == delim || c == '\n') {
+            if let Ok(number) = item.parse::<i32>() {
+                if number < 0 {
+                    return Err(CalculatorError::NegativeNumber(number));
+                }
+
+                if number > 1000 {
+                    self.logger.log(number);
+                }
+                sum += number;
+            }
+        }
+        Ok(sum)
     }
-    Ok(sum)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::string_calculator::{add};
-    use crate::string_calculator::CalculatorError;
+    use super::*;
+    fn setup_calculator() -> StringCalculator<MockLogger> {
+        let mock_logger = MockLogger::new();
+        StringCalculator::new(mock_logger)
+    }
+    
     #[test]
     fn test_empty_string_returns_zero() {
-        assert_eq!(0, add("").unwrap());
+        let calculator = setup_calculator();
+        assert_eq!(0, calculator.add("").unwrap());
     }
     #[test]
     fn test_one_number_input() {
-        assert_eq!(44, add("44").unwrap());
+        let calculator = setup_calculator();
+        assert_eq!(44, calculator.add("44").unwrap());
     }
     #[test]
     fn test_two_number_input() {
-        assert_eq!(5, add("2,3").unwrap());
+        let calculator = setup_calculator();
+        assert_eq!(5, calculator.add("2,3").unwrap());
     }
     #[test]
     fn test_unknown_number_input() {
-        assert_eq!(55, add("1,2,3,4,5,6,7,8,9,10").unwrap());
+        let calculator = setup_calculator();
+        assert_eq!(55, calculator.add("1,2,3,4,5,6,7,8,9,10").unwrap());
     }
     #[test]
     fn test_handle_newlines_input() {
-        assert_eq!(6, add("1\n2,3").unwrap());
+        let calculator = setup_calculator();
+        assert_eq!(6, calculator.add("1\n2,3").unwrap());
     }
     #[test]
     fn test_various_delimiters_input() {
-        assert_eq!(3, add("//;\n1;2").unwrap());
+        let calculator = setup_calculator();
+        assert_eq!(3, calculator.add("//;\n1;2").unwrap());
     }
     #[test]
     fn test_negative_number_input() {
-        let result = add("1,-2,3");
+        let calculator = setup_calculator();
+        let result = calculator.add("1,-2,3");
         assert_eq!(result, Err(CalculatorError::NegativeNumber(-2)),
         "Negative number should result in error");
     }
